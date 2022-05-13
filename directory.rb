e9b346b933da4bb998819adba22d311452f23863
Christopher <christopher2brown@hotmail.co.uk>
2022-05-13 18:32:28 +0100

    changed students [] into {} to make it more readable

--- directory.rb
+++ directory.rb
@@ -1,15 +1,16 @@
 
 students = [
-["Dr. Hannibal Lecter", :november],
-["Darth Vader", "Nurse Ratched", :november],
-["Michael Corleone", :november],
-["Alex DeLarge", :november],
-["The Wicked Witch of the West", :november],  
-["Terminator", :november],
-["Freddy Krueger", :november],
-["The Joker", :november],
-["Joffrey Baratheon", :november],
-["Norman Bates", :november]
+{name: "Dr. Hannibal Lecter",cohort: :november},    
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone",cohort: :november},
+{name: "Alex DeLarge",cohort: :november},
+{name: "The Wicked Witch of the West",cohort: :november},  
+{name: "Terminator",cohort: :november},
+{name: "Freddy Krueger",cohort: :november},
+{name: "The Joker",cohort: :november},
+{name: "Joffrey Baratheon",cohort: :november},
+{name: "Norman Bates",cohort: :november}
 ]
 
 def print_header
@@ -19,7 +20,7 @@ end
 
 def print(students)
   students.each do |students|
-    puts "#{students[0]} (#{students[1]} cohort)"
+    puts "#{students[:name]} (#{students[:cohort]} cohort)"
    end
 end
 
@@ -27,4 +28,5 @@ def print_footer(students)
     puts "Overrall, we have #{students.count} great students"
 end
  
+
 
