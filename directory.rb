e9707db8560c18446649e14dd10758fae83a9e63
Christopher <christopher2brown@hotmail.co.uk>
2022-05-13 11:48:24 +0100

    Added list of students

---
+++ directory.rb
@@ -0,0 +1,17 @@
+ 
+#!/usr/bin/env ruby
+
+
+puts "the students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
