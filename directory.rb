524e9b7056ecfa877bbeaf3039f5b635a8d725f2
Christopher <christopher2brown@hotmail.co.uk>
2022-05-13 17:33:58 +0100

    Added array and removed student_count var - updated

--- directory.rb
+++ directory.rb
@@ -1,10 +1,16 @@
 
 students = [
-"the students of Villains Academy", "-------------", "Dr. Hannibal Lecter",
-"Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge",
-"The Wicked Witch of the West", "Terminator", "Freddy Krueger",
-"The Joker", "Joffrey Baratheon" "Norman Bates"]
+"Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
+ "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",  
+ "Terminator", "Freddy Krueger","The Joker", "Joffrey Baratheon", "Norman Bates"
+]
 
+puts "The students of Villains Academy"
+puts "-------------"
+students.each do |student|
+    puts student
+
+end
 
 puts "Overall, we have #{students.count} great students"
 
